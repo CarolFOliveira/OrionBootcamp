ffd9d100667ab22ccb96f8a88a0b748ded8ac0ac
CarolFOliveira <kaarol.ferreira@gmail.com>
2024-10-04 18:24:14 -0400

    docs: adiciona comentários no padrão TSDoc na função countVowels

--- exercicio1/scripts/exercicio1.js
+++ exercicio1/scripts/exercicio1.js
@@ -1,23 +1,30 @@
-var showWord = document.querySelector("#showWord");
-var vowels = document.querySelector("#vowels");
-var submitWord = document.querySelector("#submitWord");
-var inputWord = document.querySelector("#word");
-countVowels("JOGADOR");
+"use strict";
+const showWord = document.querySelector("#showWord");
+const vowels = document.querySelector("#vowels");
+const submitWord = document.querySelector("#submitWord");
+const inputWord = document.querySelector("#word");
+/**
+ * countVowels
+ *
+ * Função que retorna a quantidade de vogais contida na palavra inserida.
+ *
+ * @param verifyWord - a palavra a ser verificada.
+ *
+ * @returns A quantidade de vogais encontrada na palavra.
+ */
 function countVowels(verifyWord) {
-    var VOGAIS = ["A", "E", "I", "O", "U"];
-    var word = verifyWord.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
-    var count = 0;
-    for (var i = 0; i < word.length; i++) {
-        if (VOGAIS.includes(word[i])) {
-            count++;
-        }
-    }
+    const VOGAIS = ["A", "E", "I", "O", "U"];
+    let word = verifyWord.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
+    let count = word.split('').reduce((count, char) => {
+        return VOGAIS.includes(char) ? count + 1 : count;
+    }, 0);
     showWord.textContent = verifyWord;
-    vowels.textContent = "".concat(count, " vogal(is)");
+    vowels.textContent = `${count} vogal(is)`;
     return count;
 }
-submitWord.addEventListener('click', function (event) {
+countVowels("JOGADOR");
+submitWord.addEventListener('click', (event) => {
     event.preventDefault();
-    var word = inputWord.value.toUpperCase();
+    const word = inputWord.value.toUpperCase();
     countVowels(word);
 });
